389b95646399d9f3490b47b871f01e540074e0b7
jc <danska@kth.se>
2018-07-13 20:34:56 +0200

    --allow-empty-message

---
+++ search.py
@@ -0,0 +1,17 @@
+import collections
+
+#https://codereview.stackexchange.com/a/135160	
+def breadth_first_search(graph, root): 
+    visited, queue = set(), collections.deque([root])
+    while queue: 
+        vertex = queue.popleft()
+        for neighbour in graph[vertex]: 
+            if neighbour not in visited: 
+                visited.add(neighbour) 
+                queue.append(neighbour) 
+
+"""
+if __name__ == '__main__':
+    graph = {0: [1, 2], 1: [2], 2: []} 
+    breadth_first_search(graph, 0)
+"""
