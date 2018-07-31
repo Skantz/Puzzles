1dcd915bb5b86130cb564e03a634b9dc6ebdea1c
jc <danska@kth.se>
2018-07-31 17:11:23 +0200

    update

--- anagramcounting.py
+++ anagramcounting.py
@@ -6,9 +6,7 @@ import operator
 
 def anagram_count(string):
   characters = list(string)
-  uniques = {}
-  for c in characters:
-    uniques[c] = 1
+  uniques = {c: 1 for c in characters}
   factorial_of_counts = [factorial(characters.count(c)) for c in uniques.keys()]
   return factorial(len(string))//reduce(operator.mul, factorial_of_counts, 1)
 
