aa173d7036b2e7f1061be98f7c1028bfe9f831b0
jc <danska@kth.se>
2018-07-31 17:58:51 +0200

    done

--- search.py
+++ search.py
@@ -15,3 +15,30 @@ if __name__ == '__main__':
     graph = {0: [1, 2], 1: [2], 2: []} 
     breadth_first_search(graph, 0)
 """
+
+#from stackoverflow. Forgot the link.
+
+def bfs(graph_to_search, start, end):
+    queue = [[start]]
+    visited = set()
+
+    while queue:
+        # Gets the first path in the queue
+        path = queue.pop(0)
+
+        # Gets the last node in the path
+        vertex = path[-1]
+
+        # Checks if we got to the end
+        if vertex == end:
+            return path
+        # We check if the current node is already in the visited nodes set in order not to recheck it
+        elif vertex not in visited:
+            # enumerate all adjacent nodes, construct a new path and push it into the queue
+            for current_neighbour in graph_to_search.get(vertex, []):
+                new_path = list(path)
+                new_path.append(current_neighbour)
+                queue.append(new_path)
+
+            # Mark the vertex as visited
+            visited.add(vertex)
