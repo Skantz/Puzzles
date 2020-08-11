97ac59420f9789ccd4cefef2f3032109e9fee1be
Skantz <18464487+Skantz@users.noreply.github.com>
2020-08-11 00:00:00 +0000

    +

---
+++ solutions/anothercandies.py
@@ -0,0 +1,13 @@
+cases_n = int(input())
+
+for _ in range(cases_n):
+  _ = input()
+  n = int(input())
+  sum_ = 0
+  for _ in range(n):
+    sum_ += int(input())
+  if sum_ % n == 0:
+    print("YES")
+  else:
+    print("NO")
+
