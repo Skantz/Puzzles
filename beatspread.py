daafc445fe72e1341ef398096eb064390713b3b9
Skantz <18464487+Skantz@users.noreply.github.com>
2020-08-22 00:00:00 +0000

    +

---
+++ beatspread.py
@@ -0,0 +1,7 @@
+n = int(input())
+for _ in range(n):
+    x, y = [int(inp) for inp in input().split()]
+    if x < y or (x + y) % 2:
+        print("impossible")
+        continue
+    print((x + y)//2, x  - (x + y)//2)
