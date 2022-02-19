f23e63bb827b4374537e55ad8c0f28c7db25efe1
Skantz <18464487+Skantz@users.noreply.github.com>
2022-02-19 13:41:40 +0100

    Refactor; make monorepo for multiple sites

---
+++ leetcode/two-sum.py
@@ -0,0 +1,20 @@
+class NaiveSolution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for (i, n) in enumerate(nums):
+            for (j, m) in enumerate(nums):
+                if ((i != j) and (n + m == target)):
+                    return [i, j]
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        inverse_and_index = {}
+        for (i, n) in enumerate(nums):
+            # 0 2    1 7      2 11     3 15
+            inverse_and_index[n] = i
+        for (i, n) in enumerate(nums):
+            try:
+                idx, m = inverse_and_index[target - n], target - n
+                if idx != i:
+                    return [i, idx]
+            except KeyError:
+                continue
