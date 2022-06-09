cac0b8b6ebc30ecfcce49a8d8df1ca3c9f3088a7
Skantz <18464487+Skantz@users.noreply.github.com>
2022-06-09 22:52:11 +0200

    +

---
+++ leetcode/merge-sorted-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        std::copy(nums2.begin(), nums2.end(),
+              nums1.begin() + m);
+        sort(nums1.begin(), nums1.end());
+    }
+};
