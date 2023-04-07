e93ee653e367718662fba64a75a0576554afd5f4
Skantz <18464487+Skantz@users.noreply.github.com>
2023-04-07 23:40:45 +0200

    +

---
+++ euler/p010.ml
@@ -0,0 +1,24 @@
+let rec is_prime n i =
+    if n < 2 then false
+    if n <= 2 then true
+    else if i = 1 then true
+    else
+      match (n mod i = 0) with
+      | true -> false
+      | false -> is_prime n (i - 1)
+
+let sqrt_int n =
+  let rec loop i =
+    match (i * i > n) with
+    | true -> i
+    | false -> loop (i + 1)
+  in loop 1
+
+let rec loop n i sum =
+  if n >= 2000000 then sum
+  else
+    match (is_prime n (sqrt_int n)) with
+    | true -> (loop (n + 1) (i + 1) (sum + n))
+    | false -> loop (n + 1) i sum
+
+ let main = Stdlib.print_int (loop 2 0 0)
