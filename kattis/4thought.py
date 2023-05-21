fba516fecd761a95a0d109d213924cb3aeb49469
Skantz <18464487+Skantz@users.noreply.github.com>
2023-05-21 20:30:48 +0200

    +-

--- kattis/4thought.py
+++ kattis/4thought.py
@@ -1,7 +1,10 @@
 #https://open.kattis.com/problems/4thought
 n = int(input())
 operators = ["+", "-", "*", "//"]
-calculations  = ["4 " + x_op + " 4 " + y_op + " 4 " + z_op + " 4" for x_op in operators for y_op in operators for z_op in operators]
+calculations = [
+    "4 " + x_op + " 4 " + y_op + " 4 " + z_op + " 4" for x_op in operators
+    for y_op in operators for z_op in operators
+]
 targets = []
 _ = [exec("targets.append(" + c + ")") for c in calculations]
 for _ in range(n):
