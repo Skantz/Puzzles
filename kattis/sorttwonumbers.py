c610cc05f71e15ed577c2a448af98ac5f1dba759
Skantz <18464487+Skantz@users.noreply.github.com>
2023-05-21 20:37:02 +0200

    +-

--- kattis/sorttwonumbers.py
+++ kattis/sorttwonumbers.py
@@ -1,4 +1,1 @@
-x, y = input().split()
-x = int(x)
-y = int(y)
-print (str(min(x, y)) + " " + str(max(x, y)))
+(lambda xs : print(str(min(xs[0], xs[1])) + " " + str(max(xs[0], xs[1]))))([int(x) for x in input().split()])
