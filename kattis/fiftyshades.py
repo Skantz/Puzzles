1ee0486e35e673fe1422b2a89ead39486ce27df2
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-04 15:57:31 +0200

    +-

--- kattis/fiftyshades.py
+++ kattis/fiftyshades.py
@@ -1,4 +1,1 @@
-#https://open.kattis.com/problems/fiftyshades
-c = len([e for e in [input().strip().lower() for _ in range(int(input()))] if "rose" in e or "pink" in e])
-print(c) if c > 0 else print("I must watch Star Wars with my daughter")
-#Done
+(lambda c : print(c) if c > 0 else print("I must watch Star Wars with my daughter"))(len([e for e in [input().strip().lower() for _ in range(int(input()))] if "rose" in e or "pink" in e]))
