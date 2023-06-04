c66a5051afef098ab24ce352fa86a0291749c005
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-04 22:20:22 +0200

    +-

--- leetcode/subsets.py
+++ leetcode/subsets.py
@@ -1,6 +1,3 @@
-from typing import List
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]: 
-        return [[]] if not nums else [] + [[nums[0]]
-                + s for s in self.subsets(nums[1:])] \
-                + [[] + s for s in self.subsets(nums[1:])]
+        return [[]] if not nums else [] + [[nums[0]] + s for s in self.subsets(nums[1:])] + [[] + s for s in self.subsets(nums[1:])]
