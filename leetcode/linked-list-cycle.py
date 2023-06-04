76ab89132f2fa0d59a6eb17d5db71a9213e56fa1
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-04 22:26:03 +0200

    +-

---
+++ leetcode/linked-list-cycle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        s = set()
+        return self.hasCycle_(head, s)
+    def hasCycle_(self, head: Optional[ListNode], s) -> bool:
+        if not head:
+            return False
+        if head in s:
+            return True
+        s.add(head)
+        return self.hasCycle_(head.next, s)
+ 
