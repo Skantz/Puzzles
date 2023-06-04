5e76a71227756cac0d3c1b9f67e12170f98a079d
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-04 22:13:56 +0200

    +-

--- leetcode/element-appearing-more-than-25-in-sorted-array.py
+++ leetcode/element-appearing-more-than-25-in-sorted-array.py
@@ -1,4 +1,5 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        s = set(arr)
-        return next(c for c in s if arr.count(c) / len(arr) > 0.25)
+        s, n = set(arr), len(arr)
+        lim = n * 0.25
+        return next(c for c in s if arr.count(c) > lim)
