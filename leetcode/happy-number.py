72ee4f58da6ec8b94f67b5d9d99c08e1dfe4685c
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-04 21:48:46 +0200

    +-

--- leetcode/happy-number.py
+++ leetcode/happy-number.py
@@ -4,6 +4,4 @@ class Solution:
         while n != 1 and n not in seen:
             seen.add(n)
             n = sum([int(j)**2 for j in list(str(n))])
-        if n == 1:
-            return True
-        return False
+        return True if n == 1 else False
