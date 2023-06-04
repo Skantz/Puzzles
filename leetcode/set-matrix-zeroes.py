078dc76086175a0a297ea4f9e7f81b7266b6441d
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-04 12:18:44 +0200

    +-

--- leetcode/set-matrix-zeroes.py
+++ leetcode/set-matrix-zeroes.py
@@ -1,8 +1,5 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
         n = len(matrix)
         m = len(matrix[0])
         zeros_tup = [(i, j) for i in range(n) for j in range(m) if matrix[i][j] == 0]
