a2e4a958298e9bd76d4c3eafab96ab8734cbae5f
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-07 22:19:17 +0200

    +-

--- leetcode/number-of-even-and-odd-bits.py
+++ leetcode/number-of-even-and-odd-bits.py
@@ -1,8 +1,3 @@
 class Solution:
     def evenOddBit(self, n: int) -> List[int]:
-        s, c = [0, 0], 0
-        while n:
-            s[c] += 1 & n
-            n = n >> 1
-            c = (c + 1) % 2
-        return s
+        return [int.bit_count(n & 1365), int.bit_count(n & 2730)]
