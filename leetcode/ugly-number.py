afdfd6f612bbdcc31d81eda8fa42374132f3f58a
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-14 21:56:49 +0200

    +

---
+++ leetcode/ugly-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n < 1:
+            return False
+        if n == 1:
+            return True
+        elif n % 2 == 0:
+            return self.isUgly(n // 2)
+        elif n % 3 == 0:
+            return self.isUgly(n // 3)
+        elif n % 5 == 0:
+            return self.isUgly(n // 5)
+        return False
