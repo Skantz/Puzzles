4e5385d12a184ec075c00076b1871f3f3b199f49
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-15 21:07:36 +0200

    +

---
+++ leetcode/find-the-difference.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for i, e in enumerate(sorted(t)):
+            if i >= len(s) or sorted(s)[i] != e:
+                return e
