4116514f4ba73838d38241fb23534a186f3f315e
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-17 21:54:30 +0200

    +

---
+++ leetcode/sqrtx.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        xa = x/2
+        for _ in range(20):
+            xa = 0.5 * (xa  + x / xa)
+        return int(xa)
