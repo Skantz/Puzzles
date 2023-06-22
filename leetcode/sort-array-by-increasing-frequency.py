79082f485448a2d31059533182ed3644c0304711
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-22 21:18:37 +0200

    +-

--- leetcode/sort-array-by-increasing-frequency.py
+++ leetcode/sort-array-by-increasing-frequency.py
@@ -1,3 +1,3 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        return sorted(nums, key=(lambda x: (nums.count(x), -x)))
+        return (lambda y : sorted(nums, key=(lambda x: (y[x], -x))))(Counter(nums))
