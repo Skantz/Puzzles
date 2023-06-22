e3c8b636d6538576788cb4b03531acae7b221bb0
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-22 22:39:06 +0200

    +-

--- leetcode/valid-perfect-square.py
+++ leetcode/valid-perfect-square.py
@@ -1,7 +1,5 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        if num == 0:
-            return 0
         xnum = num/2
         for _ in range(ceil(math.log2(num) +1)):
             xnum = 0.5 * (xnum  + num / xnum)
