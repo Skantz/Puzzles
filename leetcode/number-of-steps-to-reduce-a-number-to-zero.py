91e52cb5476da4fb503eb2ac6678b47fd507b181
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-23 08:23:06 +0200

    +

---
+++ leetcode/number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        if num <= 0:
+            return 0
+        if num % 2 == 0:
+            return 1 + self.numberOfSteps(num//2)
+        return 1 + self.numberOfSteps(num - 1)
