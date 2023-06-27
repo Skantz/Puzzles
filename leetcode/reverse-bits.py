9fc911d46d9fa33c49bd1093ef06dab9be4dc823
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-27 20:18:58 +0200

    +

---
+++ leetcode/reverse-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        m = 0
+        for _ in range(32):
+            m = m << 1
+            if (n & 1) == 1:
+                m = m | 1
+            n = n >> 1
+        return m 
