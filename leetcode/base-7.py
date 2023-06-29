13df12bda37bf6973e0ac3c2c5ebd9afa1df32d7
Skantz <18464487+Skantz@users.noreply.github.com>
2023-06-29 20:25:02 +0200

    +

---
+++ leetcode/base-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        out = ""
+        s = 1
+        if num < 0:
+            num = - num
+            s = - 1
+        while num > 0:
+            out = out + str(num% 7)
+            num = num // 7
+        return out[::-1] if s > 0 else "-" + out[::-1]
