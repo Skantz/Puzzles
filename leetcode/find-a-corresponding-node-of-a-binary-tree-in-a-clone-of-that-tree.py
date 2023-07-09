67271a30e6efd0863059f0a9927e031fa7c1e21e
Skantz <18464487+Skantz@users.noreply.github.com>
2023-07-09 13:00:59 +0200

    +

---
+++ leetcode/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        assert(~((original is None) ^ (cloned is None)) and target)
+        if not original:
+            return None
+        if original == target:
+            return cloned
+        left = self.getTargetCopy(original.left, cloned.left, target)
+        if left:
+            return left
+        right = self.getTargetCopy(original.right, cloned.right, target)
+        return right
+
