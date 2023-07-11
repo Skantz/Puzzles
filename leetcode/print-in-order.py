b65d885f4f724864c704caaaf70275be3267c881
Skantz <18464487+Skantz@users.noreply.github.com>
2023-07-11 20:24:54 +0200

    +-

--- leetcode/print-in-order.py
+++ leetcode/print-in-order.py
@@ -1,26 +1,24 @@
 class Foo:
     def __init__(self):
         first, second = False, False
-        import time
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
-
         printFirst()
         self.first = True
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
-
         if self.first == True:
             printSecond()
             self.second = True
         else:
-            time.sleep(0.01)
+            for _ in range(100):
+                pass
             return self.second(printSecond)
 
     def third(self, printThird: 'Callable[[], None]') -> None:
-
         if self.second == True:
             printThird()
         else:
-            time.sleep(0.01)
+            for _ in range(100):
+                pass
             return self.third(printThird)
