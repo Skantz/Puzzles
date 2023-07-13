062fb20282cd5b7a9a989e77fb65298050db4a94
Skantz <18464487+Skantz@users.noreply.github.com>
2023-07-13 21:28:12 +0200

    +

---
+++ leetcode/balanced-binary-tree.py
@@ -0,0 +1,10 @@
+class Solution:
+    def depth(self, root):
+        if not root:
+            return 0
+        return 1 + max(self.depth(root.left), self.depth(root.right))
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return abs(self.depth(root.left) - self.depth(root.right)) <= 1 \
+          and self.isBalanced(root.left)  and self.isBalanced(root.right)
