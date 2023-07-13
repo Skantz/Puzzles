9b50a10d78996c96e9dce0b6b987e97ebae445cf
Skantz <18464487+Skantz@users.noreply.github.com>
2023-07-13 22:18:24 +0200

    +-

--- leetcode/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ leetcode/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
         try:
-            return next(i + 1 for i, s in enumerate(sentence.split(" ")) if searchWord in s)
+            return next(i + 1 for i, s in enumerate(sentence.split(" ")) if searchWord == s[:len(searchWord)])
         except StopIteration:
             return -1
