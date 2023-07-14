97127782027ed9598557747f40883e660f68297f
Skantz <18464487+Skantz@users.noreply.github.com>
2023-07-14 21:46:32 +0200

    +

--- three-consecutive-odds.py
+++ three-consecutive-odds.py
@@ -1,13 +1,10 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        for i in range(len(arr)):
+        for i in range(len(arr) - 2):
             a = True
             for j in range(i, i + 3):
-                try:
-                    if arr[j] % 2 == 0:
-                        a = False
-                except IndexError:
-                    return False
+                if arr[j] % 2 == 0:
+                    a = False
             if a:
                 return True
         return False
