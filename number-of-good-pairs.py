376fdc105e78c311c320b058ed32a9769b3138ed
Skantz <18464487+Skantz@users.noreply.github.com>
2023-07-14 21:10:21 +0200

    +-

--- number-of-good-pairs.py
+++ number-of-good-pairs.py
@@ -6,3 +6,12 @@ class Solution:
                 if nums[i] == nums[j]:
                     ans += 1
         return ans
+    def numIdenticalPairs_(self, nums: List[int]) -> int:
+        # Slower
+        from collections import Counter
+        c = Counter(nums)
+        s = 0
+        for e in c:
+            if c[e] > 1:
+                s += (c[e] * (c[e] - 1))//2
+        return s
