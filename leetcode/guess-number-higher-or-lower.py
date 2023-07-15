77eaf64f043c9db6f71fdd7ac75af5f93f6563f2
Skantz <18464487+Skantz@users.noreply.github.com>
2023-07-15 11:04:41 +0200

    +

---
+++ leetcode/guess-number-higher-or-lower.py
@@ -0,0 +1,16 @@
+from random import randint
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        lb = 1
+        ub = n
+        while True:
+            g = randint(lb, ub)
+            ans   = guess(g)
+            if ans == 0:
+                return g
+            elif ans < 0:
+                ub = g
+            elif ans > 0:
+                lb = g
+            else:
+                assert(False)
