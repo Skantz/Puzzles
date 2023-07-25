6c93166215c424a24666abdf0ed1dad0b562d1d4
Skantz <18464487+Skantz@users.noreply.github.com>
2023-07-25 21:05:22 +0200

    +

---
+++ leetcode/find-the-prefix-common-array-of-two-arrays.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        a = set()
+        b = set()
+        c = 0
+        cs = [-1 for _ in range(len(A))]
+        assert(len(A) == len(B))
+        for i in range(len(A)):
+            if A[i] not in a:
+                if A[i] in b or A[i] == B[i]:
+                    c += 1
+                a.add(A[i])
+            if B[i] not in b and not (B[i] == A[i]):
+                if B[i] in a:
+                    c += 1
+                b.add(B[i])
+            cs[i] = c
+        return cs
