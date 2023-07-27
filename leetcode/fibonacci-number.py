1d03b5de35ecb1ecd0e8ced0eb7fba2a3737697c
Skantz <18464487+Skantz@users.noreply.github.com>
2023-07-27 21:25:14 +0200

    +-

--- leetcode/fibonacci-number.py
+++ leetcode/fibonacci-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        return self.fib(n - 1) + self.fib(n - 2)
+        match n:
+            case 0 | 1:
+                return n
+            case _:
+                return self.fib(n - 1) + self.fib(n - 2)
