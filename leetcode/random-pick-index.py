d24ce784e1cdf0a30b09131e2411c1044362137e
Skantz <18464487+Skantz@users.noreply.github.com>
2023-07-29 14:50:05 +0200

    +-+

--- leetcode/random-pick-index.py
+++ leetcode/random-pick-index.py
@@ -1,4 +1,4 @@
-class Solution:
+class NaiveSolution:
     from random import randint
     def __init__(self, nums: List[int]):
         self.nums=nums
@@ -8,3 +8,19 @@ class Solution:
         m = randint(0, n - 1)
         c = 0
         return [i for i, n in enumerate(self.nums) if (n == target)][m]
+class Solution:
+    from random import randint
+    def __init__(self, nums: List[int]):
+        self.nums=nums
+        self.idxs = {}
+        for i, e in enumerate(nums):
+            try:
+                self.idxs[e].append(i)
+            except KeyError:
+                self.idxs[e] = [i]
+
+    def pick(self, target: int) -> int:
+        n = len(self.idxs[target])
+        m = randint(0, n - 1)
+        c = 0
+        return self.idxs[target][m]
