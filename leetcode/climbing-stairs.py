ffc012f3e5be8b16b3612490b39632cece5b184c
Skantz <18464487+Skantz@users.noreply.github.com>
2023-08-04 21:56:58 +0200

    +

---
+++ leetcode/climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    mems = {1: 1, 2: 2}
+    def climbStairs(self, n: int) -> int:
+        if n < 1:
+            return 0
+        if n in self.mems.keys():
+            return self.mems[n]
+        x = self.climbStairs(n - 1)
+        y = self.climbStairs(n - 2)
+        self.mems[n] = x + y
+        return x + y
