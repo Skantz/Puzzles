3507866c4565e655182d66b130d60cb577ef2d7b
Skantz <18464487+Skantz@users.noreply.github.com>
2023-08-08 20:37:46 +0200

    +

---
+++ leetcode/maximum-subarray.py
@@ -0,0 +1,9 @@
+class Solution: #kadane
+    def maxSubArray(self, nums: List[int]) -> int:
+        if all([e < 0 for e in nums]):
+            return max(nums)
+        maxx, sum_ = 0, 0
+        for e in nums:
+            sum_ = max(0, sum_ + e)
+            maxx = max(maxx, sum_)
+        return maxx
