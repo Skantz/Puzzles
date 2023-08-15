52bac849c3026f94b7040c5194821c1ca9d11e6a
Skantz <18464487+Skantz@users.noreply.github.com>
2023-08-15 20:24:04 +0200

    +

---
+++ leetcode/implement-rand10-using-rand7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rand10(self):
+        l = False
+        i = rand7()
+        while i > 6:
+            i = rand7()
+        j = rand7()
+        while j > 5:
+            j = rand7()
+        if i < 4:
+            return j
+        else:
+            return j + 5
