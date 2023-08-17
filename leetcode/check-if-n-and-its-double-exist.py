ffbd59ecdbd5fa59dd83d7101eaa29aeb78428f9
Skantz <18464487+Skantz@users.noreply.github.com>
2023-08-17 22:01:01 +0200

    +

--- leetcode/check-if-n-and-its-double-exist.py
+++ leetcode/check-if-n-and-its-double-exist.py
@@ -1,7 +1,3 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        for i in range(len(arr)):
-            for j in range(len(arr)):
-                if i != j and arr[i] == 2 * arr[j] :
-                    return True
-        return False
+        return any (i != j and arr[i] == 2 * arr[j] for i in range(len(arr)) for j in range(len(arr)))
