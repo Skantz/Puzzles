b884b9461e4c97073c6c7bcb218af0f402fe8d1b
Skantz <18464487+Skantz@users.noreply.github.com>
2023-08-18 22:06:41 +0200

    +

---
+++ leetcode/minimum-absolute-sum-difference.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minAbsoluteSumDiff(self, nums1: List[int], nums2: List[int]) -> int:
+        min_ = sum(abs(nums1[i] - nums2[i]) for i in range(len(nums1)))
+        maxx = 0
+        for i in range(len(nums1)):
+            z = abs(nums1[i] - nums2[i])
+            if z < maxx:
+                continue
+            for j in range(len(nums2)):
+                z_ = abs(nums1[j] - nums2[i])
+                if z_ < z:
+                    maxx = max(maxx, z - z_)
+        return (min_ - maxx) % (10**9 + 7)
