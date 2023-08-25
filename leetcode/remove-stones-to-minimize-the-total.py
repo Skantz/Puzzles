8b4ea911b68a50eee4e2271f73895a1cd8d39bed
Skantz <18464487+Skantz@users.noreply.github.com>
2023-08-25 20:01:16 +0200

    +

---
+++ leetcode/remove-stones-to-minimize-the-total.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        from queue import PriorityQueue
+        q = PriorityQueue()
+        for i, e in enumerate(piles):
+            q.put((-e, i))
+        for _ in range(k):
+            e, i = q.get()
+            q.put(((e - (int(e / 2)), i)))
+        s = 0
+        while not q.empty():
+            e, i = q.get()
+            s -= e
+        return s
