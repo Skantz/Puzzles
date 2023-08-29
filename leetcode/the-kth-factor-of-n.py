65543f849638bc79b85802a92ba2297b2e5b271b
Skantz <18464487+Skantz@users.noreply.github.com>
2023-08-29 21:38:49 +0200

    5.74% 99.66%

---
+++ leetcode/the-kth-factor-of-n.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        s = 0
+        for i in range(1, n**2 + 1):
+            if n % i == 0:
+                s += 1
+            if s == k:
+                return i
+        return -1
