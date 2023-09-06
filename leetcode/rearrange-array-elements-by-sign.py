abd96dd33c69a04dce8337a708998f25befa41af
Skantz <18464487+Skantz@users.noreply.github.com>
2023-09-06 21:54:47 +0200

    +-

--- leetcode/rearrange-array-elements-by-sign.py
+++ leetcode/rearrange-array-elements-by-sign.py
@@ -1,7 +1,7 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         a = [n for n in nums if n < 0]
-        b = [n for n in nums if not n < 0]
+        b = [n for n in nums if n >= 0]
         c = []
         for i, _ in enumerate(b):
             c.append(b[i])
