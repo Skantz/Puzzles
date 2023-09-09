417b4c959d6f83027e44bbd11b3f4daf2a3fbfd1
Skantz <18464487+Skantz@users.noreply.github.com>
2023-09-09 18:34:58 +0200

    +

---
+++ leetcode/array-of-doubled-pairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canReorderDoubled(self, arr: List[int]) -> bool:
+        if not arr:
+            return True
+        a = [-e for e in arr if e < 0]
+        b = [e for e in arr if e >= 0]
+        if len(a) > 0:
+            return self.canReorderDoubled(a) and self.canReorderDoubled(b)
+        arr.sort()
+        while arr:
+            try:
+                i = arr.index(2 * arr[0])
+            except:
+                return False
+            arr = (arr[1:i] + arr[i + 1:])
+        return True
