2c76039c2cb9f81ea9b1600587bc8135f576c070
Skantz <18464487+Skantz@users.noreply.github.com>
2023-09-11 21:01:53 +0200

    0-

--- leetcode/stone-game.py
+++ leetcode/stone-game.py
@@ -1,6 +1,3 @@
 class Solution:
-    def stoneGame_(self, piles: List[int]) -> bool:
-        piles = sorted(piles)[::-1]
-        return sum(piles[::2]) >= sum(piles[1::2])
     def stoneGame(self, piles: List[int]) -> bool:
         return True
