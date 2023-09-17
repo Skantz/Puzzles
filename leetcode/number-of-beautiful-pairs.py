2443089c8d119d258b5caf4b09de39c668ce1207
Skantz <18464487+Skantz@users.noreply.github.com>
2023-09-17 13:14:03 +0200

    +-

--- leetcode/number-of-beautiful-pairs.py
+++ leetcode/number-of-beautiful-pairs.py
@@ -1,6 +1,9 @@
 class Solution:
-    from math import gcd
+    
+    def gcd(self, n, m): #euclid
+        return n if m < 1 else self.gcd(m, n % m)
+
     def countBeautifulPairs(self, nums: List[int]) -> int:
         fst = lambda n: int(str(n)[0])
         snd = lambda n: int(str(n)[-1])
-        return len([(i, j) for i in range(len(nums)) for j in range(i + 1, len(nums)) if gcd(fst(nums[i]), snd(nums[j])) == 1])
+        return len([(i, j) for i in range(len(nums)) for j in range(i + 1, len(nums)) if self.gcd(fst(nums[i]), snd(nums[j])) == 1])
