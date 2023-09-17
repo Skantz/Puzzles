02e5867adbb443ae60a607beab38e05c2932a436
Skantz <18464487+Skantz@users.noreply.github.com>
2023-09-17 12:02:05 +0200

    +

---
+++ leetcode/distinct-prime-factors-of-product-of-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def is_prime(self, n):
+        if n < 2:
+            return False
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+    
+    def divisors(self, n):
+        return [i for i in range(1, n + 1) if n % i == 0]
+
+    def distinctPrimeFactors(self, nums: List[int]) -> int:
+        ps = set()
+        for n in nums:
+            for d in self.divisors(n):
+                if self.is_prime(d):
+                    ps.add(d)
+        return len(ps)
