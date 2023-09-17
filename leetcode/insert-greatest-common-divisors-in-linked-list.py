d8e264dd8143d1da0d8e8071cbf60e2f2da70d95
Skantz <18464487+Skantz@users.noreply.github.com>
2023-09-17 13:16:24 +0200

    +-

--- leetcode/insert-greatest-common-divisors-in-linked-list.py
+++ leetcode/insert-greatest-common-divisors-in-linked-list.py
@@ -1,11 +1,15 @@
 class Solution:
-    from math import gcd
+
+    def gcd(self, n, m): #euclid
+        return n if m < 1 else self.gcd(m, n % m)
+
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
         assert head
         saved_head = head
         while head.next:
             nxt = head.next
-            gcd_ = gcd(head.val, head.next.val)
+            nxtnxt = nxt.next
+            gcd_ = self.gcd(head.val, head.next.val)
             head.next = ListNode(gcd_, nxt)
             head = nxt
         return saved_head
