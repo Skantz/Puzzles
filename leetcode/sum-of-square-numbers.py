45312a8c5b4b7d39db0e7e33ed48838a8dbaadd6
Skantz <18464487+Skantz@users.noreply.github.com>
2023-10-24 20:04:51 +0200

    +-

--- leetcode/sum-of-square-numbers.py
+++ leetcode/sum-of-square-numbers.py
@@ -1,9 +1,8 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        from math import sqrt
         if c == 0:
             return True
         for i in range(1, int(sqrt(c) + 1)):
-            if sqrt(c - i**2) == int(sqrt(c - i**2)):
+            if (c - i**2)**0.5 == int((c - i**2)**0.5):
                 return True
         return False
