08c5b5043ef2162922d67fdf9f4c527303ca89b4
Skantz <18464487+Skantz@users.noreply.github.com>
2023-11-01 20:06:26 +0100

    +

---
+++ leetcode/sum-of-subarray-ranges.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subArrayRanges(self, nums: List[int]) -> int:
+        n = len(nums)
+        s = 0
+        for i in range(n):
+            maxx = - 10**10
+            minn = 10 ** 10
+            for j in range(i + 1, n):
+                maxx = max(maxx, nums[i], nums[j])
+                minn = min(minn, nums[i], nums[j])
+                s += (maxx - minn)
+        return s
