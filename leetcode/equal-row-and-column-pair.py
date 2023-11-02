40227724d17c7d0c0adac011c10e3f8f37b90a82
Skantz <18464487+Skantz@users.noreply.github.com>
2023-11-02 20:00:36 +0100

    +

---
+++ leetcode/equal-row-and-column-pair.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def equalPairs(self, grid):
+        n = len(grid)
+        m = len(grid[0])
+        transpose = [[grid[i][j] for i in range(n)] for j in range(n)]
+        s = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i] == transpose[j]:
+                    s += 1
+        return s
