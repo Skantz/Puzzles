7b2f37252cc5cab4e9938ee5fdf7b25e638cac54
Skantz <18464487+Skantz@users.noreply.github.com>
2023-11-02 19:38:11 +0100

    +

---
+++ leetcode/minimum-sum-of-mountrain-triplets-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumSum(self, nums: List[int]) -> int:
+        minn = 50 * 3 + 1
+        b = False
+        for i, e1 in enumerate(nums):
+            for j, e2 in enumerate(nums):
+                for k, e3 in enumerate(nums):
+                    if i < j and j < k and e1 < e2 and e3 < e2:
+                        b = True
+                        minn = min(minn, e1 + e2 + e3)
+        return minn if b else -1
