eb97ec7fd3e50ed029456c835185c6d1757bc305
Skantz <18464487+Skantz@users.noreply.github.com>
2023-11-04 19:35:45 +0100

    +

---
+++ leetcode/validate-binary-search-tree.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode], range=(-2**31 - 1, 2**31 + 1)) -> bool:
+        if not root:
+            return True
+        if root.val <= range[0] or range[1] <= root.val:
+            return False
+        c1 = self.isValidBST(root.left, (range[0], root.val))
+        c2 = self.isValidBST(root.right, (root.val, range[1]))
+        return c1 and c2
