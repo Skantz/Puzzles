d7e9447793dd85bf2eecf0f58b3d8af1d68c6b1a
Skantz <18464487+Skantz@users.noreply.github.com>
2023-11-06 20:00:53 +0100

    +

---
+++ leetcode/arithmetic-subarrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        rets = []
+        for i, j in zip(l, r):
+            if j - i < 2:
+                rets.append(True)
+                continue
+            lst = nums[i:j + 1]
+            lst.sort()
+            d = lst[1] - lst[0]
+            rets.append(all(lst[i + 1] - lst[i] == d for i in range(len(lst) - 1)))
+        return rets
