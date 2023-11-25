8ece81491fb4f81739c23a34148dcf787557642b
Skantz <18464487+Skantz@users.noreply.github.com>
2023-11-25 20:00:53 +0100

    +-

--- euler/p001.sh
+++ euler/p001.sh
@@ -2,12 +2,12 @@
 SUM=0
 for i in $(seq 1 999)
 do
-  A=$(($i % 3))
-  B=$(($i % 5))
+  A=$((i % 3))
+  B=$((i % 5))
   if [ "$A" = 0 ]; 
-  then SUM=$(expr $SUM + $i)
+  then SUM=$((SUM + i))
   elif [ "$B" = 0 ];
-  then SUM=$(expr $SUM + $i)
+  then SUM=$((SUM + i))
   fi
 done
-echo $SUM
+echo "$SUM"
