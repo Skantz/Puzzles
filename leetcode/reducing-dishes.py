2e09f8b95d8b608cd4d70d468f4a0cf34e3e0d59
Skantz <18464487+Skantz@users.noreply.github.com>
2023-11-27 20:01:17 +0100

    +

---
+++ leetcode/reducing-dishes.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        st = satisfaction
+        st.sort()
+        partsums = []
+        i = 0
+        s = 0
+        removes = []
+        while True:
+            e = st[i]
+            if len(removes) == len(st):
+                assert all(e < 0 for e in st)
+                return 0
+            if i in removes:
+                i += 1
+                continue
+            if e < 0 and sum(st[i + 1:]) < -e:
+                removes.append(i)
+                i = 0
+                continue
+            i += 1
+            if len(st) < i + 1:
+                break
+        print(st)
+        print(removes)
+        return sum(e * (i + 1) for i, e in enumerate([f for j, f in enumerate(st) if j not in removes]))
