d2adfe55bbd68cc872cddb1915d194b3f39a61b3
Skantz <18464487+Skantz@users.noreply.github.com>
2024-02-20 20:00:12 +0100

    +

---
+++ leetcode/subarray-sum-equals-k.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        vector<int> runsum;
+        int s = 0;
+        runsum.push_back(0);
+        for (auto e: nums) {
+            s += e;
+            runsum.push_back(s);
+        }
+        int ret = 0;
+        /* improve : 1 pass */
+        for (unsigned int i = 0; i < runsum.size() + 1; i++) {
+            for (unsigned int j = i + 1; j < runsum.size() + 1; j++) {
+                if ((runsum[j] - runsum[i]) == k) {
+                    ret++;
+                }
+            }
+        }
+        return ret;
+    }
+};
