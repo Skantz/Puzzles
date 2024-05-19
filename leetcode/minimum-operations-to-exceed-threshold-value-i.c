33d7c156cd8ec6dcc8d5419ff24df607184d957e
Skantz <18464487+Skantz@users.noreply.github.com>
2024-05-19 20:00:52 +0200

    +-

--- leetcode/minimum-operations-to-exceed-threshold-value-i.c
+++ leetcode/minimum-operations-to-exceed-threshold-value-i.c
@@ -1,10 +1,9 @@
 int minOperations(int* nums, int nsz, int k) {
     int s = 0;
     for (int i = 0; i < nsz; i++) {
-        if (*nums < k) {
+        if (*nums++ < k) {
             s++;
         }
-        nums++;
     }
     return s;
 }
