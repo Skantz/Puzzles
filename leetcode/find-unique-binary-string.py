624082fb65c0ab4d251532748d2c2de0297421ca
Skantz <18464487+Skantz@users.noreply.github.com>
2024-06-11 20:13:03 +0200

    +

--- leetcode/find-unique-binary-string.py
+++ leetcode/find-unique-binary-string.py
@@ -1,10 +1,13 @@
+from typing import List
+
+
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
+        # cantor diagonalization
         n = len(nums)
-        ret = ""
-        for i in range(n):
-            if nums[i][i] == "1":
-                ret += "0"
-            else:
-                ret += "1"
-        return ret
+        m = len(nums)
+        assert all([len(e) == m for e in nums])
+        out = ""
+        for i in range(min(n, m)):
+            out += "1" if nums[i][i] == "0" else "0"
+        return out
