cd6475f3965330b126b80b74502b1a069439ad23
Skantz <18464487+Skantz@users.noreply.github.com>
2024-07-13 20:26:10 +0200

    +

--- leetcode/kth-largest-element-in-an-array.py
+++ leetcode/kth-largest-element-in-an-array.py
@@ -1,6 +1,10 @@
+from typing import List
+
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return sorted(nums)[::-1][k - 1]
+        return sorted(nums)[-k]
+
     def findKthLargest_(self, nums: List[int], k: int) -> int:
         from queue import PriorityQueue
         q = PriorityQueue()
