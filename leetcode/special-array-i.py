8e045811e22c6c3cda045312caf827feecfa6fa4
Skantz <18464487+Skantz@users.noreply.github.com>
2024-08-02 20:00:53 +0200

    +

--- leetcode/special-array-i.py
+++ leetcode/special-array-i.py
@@ -1,3 +1,3 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        return all([(e & 1) ^ 1 != (nums[i + 1] & 1 ^ 1) for i, e in enumerate(nums[:-1])])
+        return all([(e & 1) != (nums[i + 1] & 1) for i, e in enumerate(nums[:-1])])
