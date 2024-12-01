4ed3bcc1ba83b8f0c40ba17451af84436b6372cd
Skantz <18464487+Skantz@users.noreply.github.com>
2024-12-01 20:00:51 +0100

    +

--- leetcode/contains-duplicate-ii.py
+++ leetcode/contains-duplicate-ii.py
@@ -1,11 +1,16 @@
-from typing import List
-
-
 class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+    def containsNearbyDuplicate(self, nums, k: int) -> bool:
+        """
         n = len(nums)
         for i in range(n):
             for j in range(i + 1, n):
                 if abs(i - j) <= k and nums[i] == nums[j]:
                     return True
+        return False
+        """
+        mapp = dict()
+        for i, e in enumerate(nums):
+            if e in mapp and abs(i) - mapp[e] - 1 < k:
+                return True
+            mapp[e] = i
         return False
