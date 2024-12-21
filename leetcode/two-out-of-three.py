855f5f53b95c9c9120a4a5021a43e3ed7f6e74ad
Skantz <18464487+Skantz@users.noreply.github.com>
2024-12-21 19:58:17 +0100

    +-

--- leetcode/two-out-of-three.py
+++ leetcode/two-out-of-three.py
@@ -1,4 +1,4 @@
 class Solution:
-    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
-        A, B, C = set(nums1), set(nums2), set(nums3)
-        return list((A & B) | (A & C) | (B & C))
+    def twoOutOfThree(self, X, Y, Z):
+        X, Y, Z = set(X), set(Y), set(Z)
+        return list((X & Y) | (X & Z) | (Y & Z))
