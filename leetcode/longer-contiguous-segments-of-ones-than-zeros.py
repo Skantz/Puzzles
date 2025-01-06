fa0bb94498cb1867d76ad654ea7b9f0e2726608e
Skantz <18464487+Skantz@users.noreply.github.com>
2025-01-06 20:00:24 +0100

    +-

--- leetcode/longer-contiguous-segments-of-ones-than-zeros.py
+++ leetcode/longer-contiguous-segments-of-ones-than-zeros.py
@@ -1,6 +1,10 @@
 class Solution:
     def checkZeroOnes(self, s: str) -> bool:
-        try:
-            return sorted(s).index('1') < len(s)/2
-        except ValueError:
-            return False
+        for i in range(1, 100 + 1):
+            x = '0' * i in s
+            y = '1' * i in s
+            if (x and not y) or (not x and not y):
+                return False
+            if not x:
+                return True
+        assert False
