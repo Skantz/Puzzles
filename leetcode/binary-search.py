8eb808e34348fd71925f3db2e4d3c17f02842f72
Skantz <18464487+Skantz@users.noreply.github.com>
2025-01-11 20:00:57 +0100

    +-

--- leetcode/binary-search.py
+++ leetcode/binary-search.py
@@ -1,23 +1,20 @@
 class Solution:
-    def bisect_left(self, lst, x, lb=None, ub=None):
-        if len(lst) < 1:
-            return -1
-        if lb is None:
-            assert ub is None
-            lb, ub = 0, len(lst) - 1
+    def bisect_left(self, f, x, lb, ub):
+        assert 0 <= lb <= ub
         if lb == ub:
             return lb
         if ub - lb < 2:
-            return lb if x < lst[ub] else ub
+            return lb if x < f(ub) else ub
         idx = lb + (ub - lb)//2 + (ub - lb) % 2
         # assert (idx - 1 < ub)
-        y = lst[idx]
+        y = f(idx)
         if y < x:
-            return self.bisect_left(lst, x, idx, ub)
-        return self.bisect_left(lst, x, lb, idx)
+            return self.bisect_left(f, x, idx, ub)
+        return self.bisect_left(f, x, lb, idx)
 
     def search(self, lst, x):
-        i = self.bisect_left(lst, x)
+        def f(x): return lst[x]
+        i = self.bisect_left(f, x, 0, len(lst) - 1)
         if i == len(lst) or lst[i] != x:
             return -1
         return i
